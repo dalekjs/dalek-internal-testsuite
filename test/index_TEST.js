e5aedee67d9a8adaad85ea798b46339d963d4913
asciidisco <public@asciidisco.com>
2013-07-09 21:35:10 +0200

    Tests

--- test/index_TEST.js
+++ test/index_TEST.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var expect = require('chai').expect;
-var Assertions = require('../index.js');
+var Testsuite = require('../index.js');
 
-describe('dalek-internal-assertions', function() {
+describe('dalek-internal-testsuite', function() {
 
   it('should exist', function() {
-    var assertions = new Assertions();
-    expect(assertions).to.be.ok;
+    expect(Testsuite).to.be.ok;
   });
 
 });
